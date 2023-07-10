f2dd4866066d0b80ad77433d11742be7abfd1ad7
mzwierzchowski <mzwierzchowski@deloittece.com>
2023-07-10 16:41:21 +0200

    Update 2

--- src/main/java/com/deloitte/ads/App.java
+++ src/main/java/com/deloitte/ads/App.java
@@ -1,13 +1,23 @@
 package com.deloitte.ads;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        User mario = new User(1, "Mario", "Mariowski", new HashSet<>(), new HashSet<>());
+        User luigi = new User(2, "Luigi", "Luigiowski", new HashSet<>(), new HashSet<>());
+
+        Marios marios = new Marios(1, Marios.TypeEnum.HAPPY, "Hello", mario.getId(), luigi.getId());
+
+        mario.addSentMarios(marios.getId());
+        luigi.addReceivedMarios(marios.getId());
+
     }
 }
